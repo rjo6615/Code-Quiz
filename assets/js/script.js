d116d2cc9396d4ac5737c4312556ce8272cc714f
rjo6615 <robertjobernier@gmail.com>
2022-10-06 13:09:17 -0400

    Condesed wrong answer buttons into function

--- assets/js/script.js
+++ assets/js/script.js
@@ -11,13 +11,16 @@ var questionText = document.getElementById("question");
 var questionsPos = 0;
 var currAnswers = 0;
 var correctAnswer = 0;
+// All questions
 var questions = ["First Question", "Second Question", "Third Question", "Fourth Question"];
 
+// All question answers
 var q1answers = ["Yes", "No", "No", "No"];
 var q2answers = ["No", "Yes", "No", "No"];
 var q3answers = ["Yes", "No", "No", "No"];
 var q4answers = ["No", "No", "No", "Yes"];
 var answersArray = [q1answers, q2answers, q3answers, q4answers];
+// Position of answers according to arrays above
 var correctArray = [0, 1, 0, 3];
 var btnArray = [choiceBtn1, choiceBtn2, choiceBtn3, choiceBtn4];
 var correctChosen;
@@ -25,11 +28,15 @@ var correctChosen;
 
 // will have to run a function that starts the timer and call the writeQuestion from that
 startBtn.addEventListener("click", writeQuestion);
-choiceBtn1.addEventListener("click", wrong);
-choiceBtn2.addEventListener("click", wrong);
-choiceBtn3.addEventListener("click", wrong);
-choiceBtn4.addEventListener("click", wrong);
 
+function wrongAnswer() {
+    choiceBtn1.addEventListener("click", wrong);
+    choiceBtn2.addEventListener("click", wrong);
+    choiceBtn3.addEventListener("click", wrong);
+    choiceBtn4.addEventListener("click", wrong);
+};
+
+wrongAnswer();
 
 function writeQuestion() {
 questionText.textContent = questions[questionsPos];
@@ -44,18 +51,12 @@ var correctChoice = (correctArray[correctAnswer]);
 correctChosen = (btnArray[correctChoice]);
 correctChosen.removeEventListener("click", wrong);
 correctChosen.addEventListener("click", correct);
-// Need to find way to decide if answer is wrong or not "seems to be working as is". make all wrongs into an object.
-
-
 };
 
 function correct() {
 alert("Correct!");
 correctChosen.removeEventListener("click", correct);
-choiceBtn1.addEventListener("click", wrong);
-choiceBtn2.addEventListener("click", wrong);
-choiceBtn3.addEventListener("click", wrong);
-choiceBtn4.addEventListener("click", wrong);
+wrongAnswer();
 questionsPos++;
 currAnswers++;
 correctAnswer++;
@@ -65,10 +66,7 @@ writeQuestion();
 function wrong() {
 alert("Wrong!");
 correctChosen.removeEventListener("click", correct);
-choiceBtn1.addEventListener("click", wrong);
-choiceBtn2.addEventListener("click", wrong);
-choiceBtn3.addEventListener("click", wrong);
-choiceBtn4.addEventListener("click", wrong);
+wrongAnswer();
 questionsPos++;
 currAnswers++;
 correctAnswer++;
