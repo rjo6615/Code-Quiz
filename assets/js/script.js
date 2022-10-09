0949240e45d7c5c878b7869e125bcc5c3e6034a4
rjo6615 <robertjobernier@gmail.com>
2022-10-09 16:31:40 -0400

    reverted project code was not working properly

--- assets/js/script.js
+++ assets/js/script.js
@@ -129,14 +129,14 @@ function startCountdown(seconds) {
     textField.style.display = 'inline-block';
     highScoreBtn.addEventListener("click", saveHighScore); //submit button
  };
+ 
 
  function saveHighScore() {
+    
+    
     var initials = document.querySelector("#initials").value; //retreive value of text field
     localStorage.setItem("highscore", counter); // add counter value to local storage
     localStorage.setItem("initials", initials); // add initials to local storage
-
-
-
     startBtn.style.display = 'inline-block';
     showHighScoresBtn.style.display = 'inline-block'; 
     highScoreBtn.style.display = 'none';
@@ -154,65 +154,3 @@ function startCountdown(seconds) {
  function showHighScores() {
 
  };
-
- var todoInput = document.querySelector("#todo-text");
- var todoForm = document.querySelector("#todo-form");
- var todoList = document.querySelector("#todo-list");
- var todoCountSpan = document.querySelector("#todo-count");
- 
- var todos = [];
- init();
- 
-
- function storeTodos() {
-  localStorage.setItem("todos", JSON.stringify(todos));
-}
-todoForm.addEventListener("submit", function(event) {
-  event.preventDefault();
-  var todoText = todoInput.value.trim();
-  if (todoText === "") {
-    return;
-  }
-  todos.push(todoText);
-  todoInput.value = "";
-  storeTodos();
-  renderTodos();
-});
-
-function init() {
-  var storedTodos = JSON.parse(localStorage.getItem("todos"));
-  if (storedTodos !== null) {
-    todos = storedTodos;
-  }
-  renderTodos();
-}
-
- function renderTodos() {
-   todoList.innerHTML = "";
-   todoCountSpan.textContent = todos.length;
-   
-   for (var i = 0; i < todos.length; i++) {
-     var todo = todos[i];
- 
-     var li = document.createElement("li");
-     li.textContent = todo;
-     li.setAttribute("data-index", i);
- 
-     var button = document.createElement("button");
-     button.textContent = "Complete ✔️";
- 
-     li.appendChild(button);
-     todoList.appendChild(li);
-   }
- }
- 
- todoList.addEventListener("click", function(event) {
-  var element = event.target;
-  if (element.matches("button") === true) {
-    var index = element.parentElement.getAttribute("data-index");
-    todos.splice(index, 1);
-    storeTodos();
-    renderTodos();
-  }
-});
- 
